f52f176c1d9863a6c0a13e04f8963dbfad747974
talesb <talesbatistafig@hotmail.com>
2020-04-05 15:39:01 -0300

    Montando arquitetura inicial do projeto

---
+++ src/main/java/br/com/sellcarplusmvc/SellcarplusmvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.sellcarplusmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellcarplusmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellcarplusmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sellcarplusmvc/SellcarplusmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sellcarplusmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellcarplusmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
