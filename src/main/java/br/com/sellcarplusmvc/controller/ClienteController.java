3cb7a35102af055e4e6f2347416fa9cf656b05f5
Tales <talesbatistafig@hotmail.com>
2020-04-06 18:42:09 -0300

    Iniciando desenvolvimento dos controladores mvc

--- src/main/java/br/com/sellcarplusmvc/controller/ClienteController.java
+++ src/main/java/br/com/sellcarplusmvc/controller/ClienteController.java
@@ -3,21 +3,46 @@ package br.com.sellcarplusmvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.sellcarplusmvc.model.negocio.Cliente;
 import br.com.sellcarplusmvc.model.service.ClienteService;
 
-@RestController
+@Controller
 @RequestMapping("/api/cliente")
 public class ClienteController {
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showInit() {	
+		return this.showLogin();
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLogin() {	
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String showHome(
+			@RequestParam String login,
+			@RequestParam String senha
+			) {
+		boolean isValid = "admin".equalsIgnoreCase(login) &&
+						  "admin".equalsIgnoreCase(senha);
+		if(!isValid) {
+			return "login";
+		}
+		return "home";
+	}
+	
 	@Autowired
 	private ClienteService clienteService;
 
--- src/main/resources/webapp/WEB-INF/jsp/login.jsp
+++ src/main/resources/webapp/WEB-INF/jsp/login.jsp
@@ -4,9 +4,13 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>login</title>
 </head>
-<body>
-
+<body>	
+	<form action="/Login" method="post">
+		Login: <input type="text" name="login">
+		Senha: <input type="password" name="senha">
+		<input type="submit" value="Acessar">
+	</form>
 </body>
 </html>
